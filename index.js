d10a20ff7311e9dc9eae5ea7a2a965055f047deb
edch <soderblomed@gmail.com>
2024-11-07 22:05:08 +0100

    change to export default

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // index.js
 
 // Function to generate a random number between min (inclusive) and max (inclusive)
-function getRandomNumber(min, max) {
+export default function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Export the function to make it accessible outside this module
-module.exports = {
-  getRandomNumber,
-};
+// module.exports = {
+//   getRandomNumber,
+// };
